e18e0e258897c515c12470326bb5155aa3ac204b
YAKI TACO <tnknet21@gmail.com>
2021-08-29 23:53:43 +0900

    [CCR]Add GUI Ranking viewing.

--- GuiCtl.cs
+++ GuiCtl.cs
@@ -1,9 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class GuiCtl : MonoBehaviour
 {
+    List<string> nameList = new List<string>();
+    public Text t;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,8 +20,25 @@ public class GuiCtl : MonoBehaviour
         
     }
     
-    public void changeRank(int id, int before, int after){
-        
+    public void addPball(string name, Color color){
+        nameList.Add(name);
+        showList();
+    }
+    
+    public void changeRank(int before, int after){
+        var cache = nameList[before];
+        nameList[before] = nameList[after];
+        nameList[after] = cache;
+        showList();
+    }
+    
+    void showList(){
+        t.text = "";
+        int i = 1;
+        foreach(var n in nameList ){
+            t.text += "[" + i +"] "+n + "\n";
+            i++;
+        }
     }
     
 }
---
+++ readPballConf.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+// ボール設定ファイルを読み取る
+public class readPballConf
+{
+    public readPballConf(string filePath){
+        if (System.IO.File.Exists(@filePath)){
+            StreamReader sr = new StreamReader(@filePath);
+            // 末尾まで繰り返す
+            while (!sr.EndOfStream) {
+                // CSVファイルの一行を読み込む
+                string line = sr.ReadLine();
+                // 読み込んだ一行をカンマ毎に分けて配列に格納する
+                string[] values = line.Split(',');
+ 
+                // 配列からリストに格納する
+                List<string> lists = new List<string>();
+                lists.AddRange(values);
+ 
+                // コンソールに出力する
+                foreach (string list in lists) {
+                    System.Console.Write("{0} ", list);
+                }
+                System.Console.WriteLine();
+            }
+            System.Console.ReadKey();
+        }
+    }
+}
